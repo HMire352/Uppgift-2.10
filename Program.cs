94fbf302d390b31ffea8b4f59d1d80ea24c7044a
Pingvu <alka04006@utb.vaxjo.se>
2022-09-09 14:15:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vilken favorit bok har du?");
+            string stad = Console.ReadLine();
+            Console.WriteLine("Ditt favorit bok är Engelska ");
+        }
+    }
+}
